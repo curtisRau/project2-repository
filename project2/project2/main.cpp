8dc685b9ee83b906f0e5bb20e777180fad679256
Curtis Rau <curtisrau@gmail.com>
2016-03-04 17:29:26 -0500

    initial commit

---
+++ project2/project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project2
+//
+//  Created by Curtis Rau on 3/4/16.
+//  Copyright © 2016 Curtis Rau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
